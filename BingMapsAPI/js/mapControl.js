29000b9fecf55fce3a7a42ac2fe3faf1da92cb35
Sergey <seruy5432@gmail.com>
2017-02-25 21:40:35 +0200

    Make a search of coffee shops via a REST request

--- BingMapsAPI/js/mapControl.js
+++ BingMapsAPI/js/mapControl.js
@@ -60,15 +60,69 @@ function ClickDataSource(credentials) {
 }
 
 function MakeDataSourceRequest(credentials) {
-    // *** Add your source code here ***
+    //Construct a REST request
+    var request =
+        "http://spatial.virtualearth.net/REST/v1/data/20181f26d9e94c81acdf9496133d4f23/FourthCoffeeSample/FourthCoffeeShops?spatialFilter=nearby('";
+    request += dataSourceInput.value; //Add input text to request
+    request += "',5.0)";               //5 km search radius
+    request += "&$select=*";          //Get all result info from source
+    request += "&$format=json";       //Return data in JSON format
+    request += "&jsonp=DataSourceCallback";//DataSourceCallback w/results
+    request += "&key=" + credentials; //Use API key for this request
+    request = encodeURI(request);
+    CallRestService(request);
 }
 
 function CallRestService(request) {
-    // *** Add your source code here ***
+    //Create a "script" where the source is our REST endpoint
+    var script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("src", request);
+
+    //Attach script to the document, which immediately runs it
+    document.body.appendChild(script);
 }
 
 function DataSourceCallback(response, userdata) {
-    // *** Add your source code here ***
+    //Clear previous pins
+    map.entities.clear();
+
+    let results = response.d.results;
+    if (results.length == 0) {
+        alert("No results found.");
+    }
+
+    //Initialize a location array to move the map to the results
+    let locations = [];
+
+    //Loop through results
+    for (var i = 0; i < results.length; i++) {
+        let result = results[i];
+
+        //Convert location data into a format we can use to move the map
+        let location = { latitude: result.Latitude, longitude: result.Longitude };
+        locations.push(location);
+
+        //Create an infobox containing display name and phone number
+        let pinInfobox = new Microsoft.Maps.Infobox(location,
+            {
+                title: result.DisplayName,
+                description: result.Phone
+            });
+
+        //Create a pushpin at the location
+        let pushpin = new Microsoft.Maps.Pushpin(location,
+            { text: (i + 1).toString(), infobox: pinInfobox });
+
+        //Add infobox and pushpin to map
+        map.entities.push(pushpin);
+        map.entities.push(pinInfobox);
+    }
+    //Pan the map if we got results
+    if (results.length > 0) {
+        //Use a bounding box constructed from all found locations
+        map.setView({ bounds: Microsoft.Maps.LocationRect.fromLocations(locations) });
+    }
 }
 
 function ClickSearch() {
